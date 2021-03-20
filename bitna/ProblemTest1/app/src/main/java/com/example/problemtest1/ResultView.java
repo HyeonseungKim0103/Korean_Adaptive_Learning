c36c4c8a0d90c9800f7ee7c22df520f721a75330
bitchnayo <65988439+bitchnayo@users.noreply.github.com>
2021-03-20 10:19:49 +0900

    빛나 것

---
+++ bitna/ProblemTest1/app/src/main/java/com/example/problemtest1/ResultView.java
@@ -0,0 +1,41 @@
+package com.example.problemtest1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.auth.User;
+
+public class ResultView extends LinearLayout {
+    private TextView resultNumber;
+    private TextView userAnswer;
+    private TextView realAnswer;
+    private TextView result;
+
+    public ResultView(Context context, UserSet mUserSet){
+        super(context);
+
+        //인플레이션
+        LayoutInflater inflater= (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.user_result,this,true);
+
+        //set icon
+        resultNumber = findViewById(R.id.resultNumber);
+        //mIcon.setImageResource(data.getIcon());
+        userAnswer = findViewById(R.id.userAnswer);
+
+        realAnswer = findViewById(R.id.realAnswer);
+
+        result = findViewById(R.id.result);;
+    }
+
+    public void setResultNumber(String data) {resultNumber.setText(data);};
+
+    public void setUserAnswer(String data) {userAnswer.setText(data);};
+
+    public void setRealAnswer(String data) {realAnswer.setText(data);}
+
+    public void setResult(String data) {result.setText(data);};
+}
